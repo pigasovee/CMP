e4307849f67b7319e61778dcfe5e39ef0994c49e
BelyaevPavel <gbelbih@gmail.com>
2020-11-09 22:21:36 +0500

    Projects created

---
+++ CMP_Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CMP_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Меняем запятые на точки в разделителях целой и дробной части
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+
+            Console.WriteLine($"Starting CMP...");
+        }
+    }
+}
