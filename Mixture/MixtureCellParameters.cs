b3e7a8b18ddd6d62cbf3ff6ba821da8def7cfd52
pigasovee <pigasovee@mail.ru>
2021-10-28 21:46:09 +0500

    Mixture and mixture parameters dev

---
+++ Mixture/MixtureCellParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LinearAlgebra;
+
+namespace Mixture
+{
+    public class MixtureCellParameters
+    {
+        double[] _pressure;
+        double[] _density;
+        double[] _totalEnergy;
+        Vector[] _velocity;
+
+        public MixtureCellParameters()
+        {
+        }
+    }
+}
